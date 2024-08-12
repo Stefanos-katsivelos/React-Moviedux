a41e522174aa5a8e7698cc1cbb8337d5e5ec96d8
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2024-08-12 21:32:34 +0300

    using states, useState & useEffect

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <Header />
+        <Header></Header>
+        <MoviesGrid></MoviesGrid>
       </div>
 
-      <Footer />
+      <Footer></Footer>
     </div>
   );
 }

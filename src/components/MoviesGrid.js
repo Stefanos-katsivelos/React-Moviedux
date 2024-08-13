2449b7f6b7c85cce5931dd6a1914624dc6dae958
Stefanos-katsivelos <stef.katsivelos@gmail.com>
2024-08-13 03:55:18 +0300

    search bar logic

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -4,6 +4,7 @@ import MovieCard from "./MovieCard";
 
 export default function MoviesGrid() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetch("movies.json")
@@ -11,11 +12,28 @@ export default function MoviesGrid() {
       .then((data) => setMovies(data));
   }, []);
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="movies-grid">
-      {movies.map((movie) => (
-        <MovieCard movie={movie} key={movie.id}></MovieCard>
-      ))}
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search Movies..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <div className="movies-grid">
+        {filteredMovies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}></MovieCard>
+        ))}
+      </div>
     </div>
   );
 }
